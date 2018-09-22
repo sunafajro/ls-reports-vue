d123afc4453541ca4d1035bfd3a03e1b628ed146
Evgeny Belkin <sunafajro@gmail.com>
2018-09-22 17:03:18 +0300

    пробрамываем параметр из исходной странички

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 
+const el = document.getElementById("app");
 Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
+  render: h =>
+    h(App, {
+      props: {
+        nullYear: parseInt(el.dataset.nullyear)
+      }
+    })
 }).$mount("#app");
