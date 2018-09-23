64d06ec51ba0415de6ce78c7cdedde92d847f4bb
Evgeny Belkin <sunafajro@gmail.com>
2018-09-23 17:08:00 +0300

    добавлено форматирование даты и суммы по дате

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10">
     <div :key="office" v-for="office in Object.keys(rows)" v-if="typeof rows === 'object' && Object.keys(rows).length">
-      <h3>{{ rows[office].name }} <small><span title="Наличные"><i class="fa fa-money" aria-hidden="true"></i> {{ rows[office].counts.cash | formatNumber  }}</span>&nbsp;&nbsp;<span title="Карта"><i class="fa fa-credit-card" aria-hidden="true"></i> {{ rows[office].counts.card | formatNumber  }}</span>&nbsp;&nbsp;<span title="Банк"><i class="fa fa-university" aria-hidden="true"></i> {{ rows[office].counts.bank | formatNumber  }}</span>&nbsp;&nbsp;<span title="Всего"><i class="fa fa-rub" aria-hidden="true"></i> {{ rows[office].counts.all | formatNumber  }}</span></small></h3>
+      <h3>
+        {{ rows[office].name }}&nbsp;&nbsp;
+        <small>
+          <span title="Наличные"><i class="fa fa-money" aria-hidden="true"></i> {{ rows[office].counts.cash | formatNumber  }}</span>&nbsp;&nbsp;
+          <span title="Карта"><i class="fa fa-credit-card" aria-hidden="true"></i> {{ rows[office].counts.card | formatNumber  }}</span>&nbsp;&nbsp;
+          <span title="Банк"><i class="fa fa-university" aria-hidden="true"></i> {{ rows[office].counts.bank | formatNumber  }}</span>&nbsp;&nbsp;
+          <span title="Всего"><i class="fa fa-rub" aria-hidden="true"></i> {{ rows[office].counts.all | formatNumber  }}</span>
+        </small>
+      </h3>
       <div :key="date" v-for="date in Object.keys(rows[office])" v-if="typeof rows === 'object' && Object.keys(rows[office]).length > 1 && date !== 'name' && date !== 'counts'">
-        <h4>{{ date }}</h4>
+        <h4>
+          {{ date | formatDate }}&nbsp;&nbsp;
+          <small>
+            <span title="Наличные"><i class="fa fa-money" aria-hidden="true"></i> {{ rows[office][date].counts.cash | formatNumber  }}</span>&nbsp;&nbsp;
+            <span title="Карта"><i class="fa fa-credit-card" aria-hidden="true"></i> {{ rows[office][date].counts.card | formatNumber  }}</span>&nbsp;&nbsp;
+            <span title="Банк"><i class="fa fa-university" aria-hidden="true"></i> {{ rows[office][date].counts.bank | formatNumber  }}</span>&nbsp;&nbsp;
+            <span title="Всего"><i class="fa fa-rub" aria-hidden="true"></i> {{ rows[office][date].counts.all | formatNumber  }}</span>
+          </small>
+        </h4>
         <table class="table table-striped table-bordered table-hover table-condensed small">
           <thead v-if="columns.length">
             <th :key="`th-${column.id}`" v-for="column in columns" v-if="column.show">{{ column.name }}</th>
           </thead>
-          <tbody v-if="rows[office][date].length">
-            <tr :class="row.active === '0' ? 'danger' : ''" :key="`tr-${row.id}`" v-for="row in rows[office][date]" >
+          <tbody v-if="Array.isArray(rows[office][date].rows) && rows[office][date].rows.length">
+            <tr :class="row.active === '0' ? 'danger' : ''" :key="`tr-${row.id}`" v-for="row in rows[office][date].rows" >
               <td :class="column.id === 'type' ? colors[row[column.id]] : ''" :key="`td-${row.id}-${column.id}`" :width="column.width" v-for="column in columns" v-if="column.show"><span v-if="row.active ==='1'">{{ row[column.id] }}</span><del v-if="row.active === '0'">{{ row[column.id] }}</del></td>
             </tr>
           </tbody>
@@ -17,12 +33,16 @@
       </div>
     </div>
     <h3 class="text-center" v-if="typeof rows === 'object' && Object.keys(rows).length > 1">
-      <span title="Наличные"><i class="fa fa-money" aria-hidden="true"></i> {{ total.cash | formatNumber }}</span>&nbsp;&nbsp;<span title="Карта"><i class="fa fa-credit-card" aria-hidden="true"></i> {{ total.card | formatNumber  }}</span>&nbsp;&nbsp;<span title="Банк"><i class="fa fa-university" aria-hidden="true"></i> {{ total.bank | formatNumber  }}</span>&nbsp;&nbsp;<span title="Всего"><i class="fa fa-rub" aria-hidden="true"></i> {{ total.all | formatNumber  }}</span>
+      <span title="Наличные"><i class="fa fa-money" aria-hidden="true"></i> {{ total.cash | formatNumber }}</span>&nbsp;&nbsp;
+      <span title="Карта"><i class="fa fa-credit-card" aria-hidden="true"></i> {{ total.card | formatNumber  }}</span>&nbsp;&nbsp;
+      <span title="Банк"><i class="fa fa-university" aria-hidden="true"></i> {{ total.bank | formatNumber  }}</span>&nbsp;&nbsp;
+      <span title="Всего"><i class="fa fa-rub" aria-hidden="true"></i> {{ total.all | formatNumber  }}</span>
     </h3>
   </div>
 </template>
 
 <script>
+import moment from "moment";
 import numeral from "numeral";
 
 export default {
@@ -55,6 +75,9 @@ export default {
     };
   },
   filters: {
+    formatDate(date) {
+      return moment(date).format('DD.MM.YYYY');
+    },
     formatNumber(value) {
       return numeral(value).format("0,0");
     }
