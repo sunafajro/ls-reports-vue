6ce632f589545d075c4b1ee710d1b3e5d7ba4c61
Evgeny Belkin <sunafajro@gmail.com>
2019-02-04 22:15:31 +0300

    хлебные крошки

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="breadcrumb">
+    <li :class="link.style" :key="'ul-' + link.id" v-for="link in links">
+        <a :href="link.url" v-if="link.url">{{ link.text }}</a>
+        <span v-if="!link.url">{{ link.text }}</span>
+    </li>
+  </ul>    
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          id: 'home',
+          style: null,
+          text: 'Главная',
+          url: '/',
+        },
+        {
+          id: 'reports',
+          style: null,
+          text: 'Отчеты',
+          url: '/report/index',
+        },
+        {
+          id: 'payments',
+          style: 'active',
+          text: 'Отчет по оплатам',
+          url: null,
+        },
+      ]
+    };
+  }
+}
+</script>
